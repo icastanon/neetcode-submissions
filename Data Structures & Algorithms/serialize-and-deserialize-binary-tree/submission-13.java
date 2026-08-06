7d4c4f04192f6a15957d8f531e2abf097f8013fc
Ivan Castanon <74114090+icastanon@users.noreply.github.com>
2026-08-06 13:18:30 -0400

    Add: serialize-and-deserialize-binary-tree - submission-13

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-13.java
@@ -0,0 +1,74 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Codec {
+    int index = 0;
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        List<TreeNode> nodeList = new ArrayList<>();
+        getPreorder(root, nodeList);
+
+        String preOrderString = nodeList
+            .stream()
+            .map(
+                (n) -> 
+                {
+                    return n == null ? "#" : String.valueOf(n.val);
+                })
+            .collect(Collectors.joining(","));
+
+        return preOrderString;
+
+        
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        String[] strings = data.split(",");
+
+        return recursive(strings);
+        
+    }
+
+    private TreeNode recursive(String[] strings){
+        if(index > strings.length || strings[index].equals("#")){
+            index++;
+            return null;
+        }
+
+        TreeNode root = new TreeNode(Integer.valueOf(strings[index]));
+        index++;
+
+        root.left = recursive(strings);
+        root.right = recursive(strings);
+
+        return root;
+
+    }
+
+    private void getPreorder(TreeNode root, List<TreeNode> nodeList){
+        if(root == null){
+            nodeList.add(null);
+            return;
+        }
+
+        nodeList.add(root);
+        getPreorder(root.left, nodeList);
+        getPreorder(root.right, nodeList);
+
+    }
+}
